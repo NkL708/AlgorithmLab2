c096845cbd46f3144a4b570368168a321aa629ce
NkL <nklnsk708@gmail.com>
2022-02-07 23:39:16 +0700

    don't done yet

--- AlgorithmLab2/main.cpp
+++ AlgorithmLab2/main.cpp
@@ -1,95 +1,84 @@
 #include "tree.h"
-#define TYPE string
+#define TYPE std::string
 
 int main() {
 	setlocale(0, "");
 	Tree<TYPE> tree;
 	Tree<TYPE>::Iterator iterator;
 	Tree<TYPE>::ReverseIterator reverseIterator;
-	int sw;
+	TYPE value;
+	int sw = 0, key;
 	bool exit = false;
-	tree.Add(10, "10", tree.root, tree.root);
-	tree.Add(9, "9", tree.root, tree.root);
-	tree.Add(12, "12", tree.root, tree.root);
-	tree.Add(7, "7", tree.root, tree.root);
-	tree.Add(8, "8", tree.root, tree.root);
-	tree.Add(11, "11", tree.root, tree.root);
-	tree.Add(13, "13", tree.root, tree.root);
 
 	while (!exit) {
-		sw = 0;
-		int key;
-		TYPE value;
-		cout << endl;
-		cout << "1. Получить размер списка" << endl;
-		cout << "2. Очистить список" << endl;
-		cout << "3. Проверить список на пустоту" << endl;
-		cout << "4. Чтение значения по ключу" << endl;
-		cout << "5. Изменить значение по ключу" << endl;
-		cout << "6. Добавить значение по ключу" << endl;
-		cout << "7. Удалить значение по ключу" << endl;
-		cout << "8. Вывести дерево в консоль" << endl;
-		cout << "9. Вывести ключи методом t_Lt_Rt" << endl;
-		cout << "10. Присвоить begin() итератору" << endl;
-		cout << "11. Присвоить end() итератору" << endl;
-		cout << "12. Вывести значение итератора" << endl;
-		cout << "13. Выполнить iterator++" << endl;
-		cout << "14. Выполнить iterator--" << endl;
-		cout << "15. Проверить, равен ли итератор begin()" << endl;
-		cout << "16. Проверить, равен ли итератор end()" << endl;
-		cout << "17. Проверить, не равен ли итератор begin()" << endl;
-		cout << "18. Проверить, не равен ли итератор end()" << endl;
-		cout << "19. Присвоить rbegin() обратному итератору" << endl;
-		cout << "20. Присвоить rend() обратному итератору" << endl;
-		cout << "21. Вывести значение обратного итератора" << endl;
-		cout << "22. Выполнить reverseIterator++" << endl;
-		cout << "23. Выполнить reverseIterator--" << endl;
-		cout << "24. Проверить, равен ли обратный итератор rbegin()" << endl;
-		cout << "25. Проверить, равен ли итератор rend()" << endl;
-		cout << "26. Проверить, не равен ли обртаный итератор rbegin()" << endl;
-		cout << "27. Проверить, не равен ли обртаный итератор rend()" << endl;
-		cout << "28. Вычисление высоты дерева" << endl;
-		cout << "29. Выход из программы" << endl;
-		cout << endl;
-		cout << "Введите номер операции: ";
-		cin >> sw;
-		system("cls");
+		std::cout << "\n\tСПИСОК ОПЕРАЦИЙ\n";
+		std::cout << "\t1. Получить размер списка\n";
+		std::cout << "\t2. Очистить список\n";
+		std::cout << "\t3. Проверить список на пустоту\n";
+		std::cout << "\t4. Чтение значения по ключу\n";
+		std::cout << "\t5. Изменить значение по ключу\n";
+		std::cout << "\t6. Добавить значение по ключу\n";
+		std::cout << "\t7. Удалить значение по ключу\n";
+		std::cout << "\t8. Вывести дерево в консоль\n";
+		std::cout << "\t9. Вывести ключи методом t_Lt_Rt\n";
+		std::cout << "\t10. Присвоить begin() итератору\n";
+		std::cout << "\t11. Присвоить end() итератору\n";
+		std::cout << "\t12. Вывести значение итератора\n";
+		std::cout << "\t13. Выполнить iterator++\n";
+		std::cout << "\t14. Выполнить iterator--\n";
+		std::cout << "\t15. Проверить, равен ли итератор begin()\n";
+		std::cout << "\t16. Проверить, равен ли итератор end()\n";
+		std::cout << "\t17. Проверить, не равен ли итератор begin()\n";
+		std::cout << "\t18. Проверить, не равен ли итератор end()\n";
+		std::cout << "\t19. Присвоить rbegin() обратному итератору\n";
+		std::cout << "\t20. Присвоить rend() обратному итератору\n";
+		std::cout << "\t21. Вывести значение обратного итератора\n";
+		std::cout << "\t22. Выполнить reverseIterator++\n";
+		std::cout << "\t23. Выполнить reverseIterator--\n";
+		std::cout << "\t24. Проверить, равен ли обратный итератор rbegin()\n";
+		std::cout << "\t25. Проверить, равен ли итератор rend()\n";
+		std::cout << "\t26. Проверить, не равен ли обртаный итератор rbegin()\n";
+		std::cout << "\t27. Проверить, не равен ли обртаный итератор rend()\n";
+		std::cout << "\t28. Вычисление высоты дерева\n";
+		std::cout << "\t29. Выход из программы\n\n";
+		std::cout << "Введите номер операции: \n";
+		std::cin >> sw;
 		switch (sw) {
 		case 1:
-			tree.PrintSize();
+			tree.printSize();
 			break;
 		case 2:
-			tree.Clear(tree.root);
+			tree.clear(tree.root);
 			break;
 		case 3:
-			cout << tree.IsEmpty();
+			std::cout << tree.isEmpty();
 			break;
 		case 4:
-			cout << "Введите ключ ";
-			cin >> key;
-			tree.Read(key, tree.root);
+			std::cout << "Введите ключ ";
+			std::cin >> key;
+			tree.read(key, tree.root);
 			break;
 		case 5:
-			cout << "Введите ключ ";
-			cin >> key;
-			cout << "Введите новое значение ";
-			cin >> value;
-			tree.Edit(key, value, tree.root);
+			std::cout << "Введите ключ ";
+			std::cin >> key;
+			std::cout << "Введите новое значение ";
+			std::cin >> value;
+			tree.edit(key, value, tree.root);
 			break;
 		case 6:
-			cout << "Введите ключ ";
-			cin >> key;
-			cout << "Введите новое значение ";
-			cin >> value;
-			tree.Add(key, value, tree.root, tree.root);
+			std::cout << "Введите ключ ";
+			std::cin >> key;
+			std::cout << "Введите новое значение ";
+			std::cin >> value;
+			tree.add(key, value, tree.root, tree.root);
 			break;
 		case 7:
-			cout << "Введите ключ ";
-			cin >> key;
-			tree.Delete(key, tree.root);
+			std::cout << "Введите ключ ";
+			std::cin >> key;
+			tree.deleteItem(key, tree.root);
 			break;
 		case 8:
-			tree.Print(tree.root);
+			tree.print(tree.root);
 			break;
 		case 9:
 			tree.t_Lt_Rt(tree.root);
@@ -101,7 +90,7 @@ int main() {
 			iterator = tree.end();
 			break;
 		case 12:
-			cout << *iterator;
+			std::cout << *iterator;
 			break;
 		case 13:
 			iterator++;
@@ -110,25 +99,25 @@ int main() {
 			iterator--;
 			break;
 		case 15:
-			cout << (iterator == tree.begin());
+			std::cout << (iterator == tree.begin());
 			break;
 		case 16:
-			cout << (iterator == tree.end());
+			std::cout << (iterator == tree.end());
 			break;
 		case 17:
-			cout << (iterator != tree.begin());
+			std::cout << (iterator != tree.begin());
 			break;
 		case 18:
-			cout << (iterator != tree.end());
+			std::cout << (iterator != tree.end());
 			break;
 		case 19:
-			reverseIterator = tree.rbegin();
+			reverseIterator = tree.rBegin();
 			break;
 		case 20:
-			reverseIterator = tree.rend();
+			reverseIterator = tree.rEnd();
 			break;
 		case 21:
-			cout << *reverseIterator;
+			std::cout << *reverseIterator;
 			break;
 		case 22:
 			reverseIterator++;
@@ -137,25 +126,25 @@ int main() {
 			reverseIterator--;
 			break;
 		case 24:
-			cout << (reverseIterator == tree.rbegin());
+			std::cout << (reverseIterator == tree.rBegin());
 			break;
 		case 25:
-			cout << (reverseIterator == tree.rend());
+			std::cout << (reverseIterator == tree.rEnd());
 			break;
 		case 26:
-			cout << (reverseIterator != tree.rbegin());
+			std::cout << (reverseIterator != tree.rBegin());
 			break;
 		case 27:
-			cout << (reverseIterator != tree.rend());
+			std::cout << (reverseIterator != tree.rEnd());
 			break;
 		case 28:
-			cout << tree.GetHeight(tree.root);
+			std::cout << tree.getHeight(tree.root);
 			break;
 		case 29:
 			exit = true;
 			break;
 		default:
-			cout << "Данного индекса не существует" << endl;
+			std::cout << "Данного индекса не существует\n";
 			break;
 		}
 	}
--- AlgorithmLab2/tree.h
+++ AlgorithmLab2/tree.h
@@ -2,12 +2,12 @@
 #include <iostream>
 #include <iomanip>
 #include <stack>
-using namespace std;
 
 template <typename T>
 class Tree
 {
-	struct Item {
+	struct Item 
+	{
 		int key;
 		T data;
 		Item* right;
@@ -15,27 +15,28 @@ class Tree
 		Item* parent;
 		Item(int key, T data, Item* right = nullptr, Item* left = nullptr, Item* parent = nullptr);
 	};
-	
-	void Print_Lt_Rt_t(Item*& root);
-	Tree<T>::Item* Lt_Rt_t(Item*& root);
+
+	void print_Lt_Rt_t(Item*& root);
+	Tree<T>::Item* lt_Rt_t(Item*& root);
 public:
 	int size = 0;
 	Item* root = nullptr;
 
 	Tree();
 	~Tree();
-	void PrintSize();
-	void Clear(Item*& root);
-	bool IsEmpty();
-	void Read(int key, Item*& root);
-	void Edit(int key, T newData, Item*& root);
-	void Add(int key, T data, Item*& root, Item*& parent);
-	void Delete(int key, Item*& root);
-	void Print(Item*& root, int indent = 0);
+	void printSize();
+	void clear(Item*& root);
+	bool isEmpty();
+	void read(int key, Item*& root);
+	void edit(int key, T newData, Item*& root);
+	void add(int key, T data, Item*& root, Item*& parent);
+	void deleteItem(int key, Item*& root);
+	void print(Item*& root, int indent = 0);
 	void t_Lt_Rt(Item*& root);
-	int GetHeight(Item*& root);
+	int getHeight(Item*& root);
 
-	class Iterator {
+	class Iterator 
+	{
 	public:
 		Item* obj;
 		T& operator*();
@@ -47,8 +48,8 @@ public:
 	Iterator begin();
 	Iterator end();
 
-	class ReverseIterator {
-
+	class ReverseIterator 
+	{
 	public:
 		Item* obj;
 		T& operator*();
@@ -57,8 +58,461 @@ public:
 		bool operator ==(const ReverseIterator& it);
 		bool operator !=(const ReverseIterator& it);
 	};
-	ReverseIterator rbegin();
-	ReverseIterator rend();
+	ReverseIterator rBegin();
+	ReverseIterator rEnd();
 };
 
-#include "tree.inl"
+template<typename T>
+inline Tree<T>::Item::Item(int key, T data, Item* right, Item* left, Item* parent)
+{
+	this->key = key;
+	this->data = data;
+	this->right = right;
+	this->left = left;
+	this->parent = parent;
+}
+
+template<typename T>
+inline Tree<T>::Tree()
+{
+
+}
+
+template<typename T>
+inline Tree<T>::~Tree()
+{
+
+}
+
+template<typename T>
+inline void Tree<T>::printSize()
+{
+	std::cout << "Текущий размер дерева равен: " << size << std::endl;
+}
+
+template<typename T>
+inline void Tree<T>::clear(Item*& root)
+{
+	if (root->left) {
+		clear(root->left);
+	}
+	if (root->right) {
+		clear(root->right);
+	}
+	root = nullptr;
+	size--;
+}
+
+template<typename T>
+inline bool Tree<T>::isEmpty() 
+{
+	if (!root) return true;
+	else return false;
+}
+
+template<typename T>
+inline void Tree<T>::read(int key, Item*& root)
+{
+	if (key == root->key) {
+		std::cout << "Данные элемента с ключом " << key << ": " << root->data << std::endl;
+	}
+	else if (key < root->key) {
+		read(key, root->left);
+	}
+	else if (key > root->key) {
+		read(key, root->right);
+	}
+}
+
+template<typename T>
+inline void Tree<T>::edit(int key, T newData, Item*& root)
+{
+	if (key == root->key) {
+		root->data = newData;
+	}
+	else if (key < root->key) {
+		edit(key, newData, root->left);
+	}	
+	else if (key > root->key) {
+		edit(key, newData, root->right);
+	}	
+}
+
+template<typename T>
+inline void Tree<T>::add(int key, T data, Item*& root, Item*& parent)
+{
+	if (!root) {
+		root = new Item(key, data);
+		if (root != parent) {
+			root->parent = parent;
+		}
+		size++;
+	}
+	else if (key == root->key) {
+		std::cout << "Данный ключ уже существует в дереве\n";
+	}
+	else if (key < root->key) {
+		add(key, data, root->left, root);
+	}	
+	else if (key > root->key) {
+		add(key, data, root->right, root);
+	}
+}
+
+template<typename T>
+inline void Tree<T>::deleteItem(int key, Item*& root)
+{
+	if (key < root->key) {
+		deleteItem(key, root->left);
+	}
+	else if (key > root->key) {
+		deleteItem(key, root->right);
+	}	
+	else if (key == root->key && !root->left && !root->right) {
+		root = nullptr;
+		size--;
+	}
+	else if (key == root->key && !root->left) {
+		root = root->right;
+		root->right = nullptr;
+	}
+	else if (key == root->key && !root->right) {
+		root = root->left;
+		root->left = nullptr;
+	}
+	else if (key == root->key) {
+		root->key = root->left->key;
+		root->data = root->left->data;
+		root->left = nullptr;
+	}
+}
+
+template<typename T>
+inline void Tree<T>::print(Item*& root, int indent)
+{
+	if (root) {
+		if (root->right) {
+			print(root->right, indent + 4);
+		}
+		if (indent) {
+			std::cout << std::setw(indent) << ' ';
+		}
+		if (root->right) {
+			std::cout << " /\n" << std::setw(indent) << ' ';
+		}
+		std::cout << root->key << ":" << root->data << "\n ";
+		if (root->left) {
+			std::cout << std::setw(indent) << ' ' << " \\\n";
+			print(root->left, indent + 4);
+		}
+	}
+	else {
+		std::cout << "Дерево пустое\n";
+	}	
+}
+
+template<typename T>
+inline void Tree<T>::t_Lt_Rt(Item*& root)
+{
+	if (!root) {
+		return;
+	}
+	std::cout << root->key << " ";
+	print_Lt_Rt_t(root->left);
+	print_Lt_Rt_t(root->right);
+}
+
+template<typename T>
+inline int Tree<T>::getHeight(Item*& root)
+{
+	// Итеративный алгоритм с методички
+	int h1 = 0, h2 = 0;
+	if (!root) {
+		return 0;
+	}
+	if (root->left) {
+		h2 = getHeight(root->left);
+	}
+	if (root->right) {
+		h1 = getHeight(root->right);
+	}
+	return (std::max(h1, h2) + 1);
+}
+
+template<typename T>
+inline void Tree<T>::print_Lt_Rt_t(Item*& root)
+{
+	if (!root) {
+		return;
+	}
+	print_Lt_Rt_t(root->left);
+	print_Lt_Rt_t(root->right);
+	std::cout << root->key << " ";
+}
+
+template<typename T>
+inline Tree<T>::Iterator Tree<T>::begin()
+{
+	Iterator it;
+	it.obj = root;
+	return it;
+}
+
+template<typename T>
+inline Tree<T>::Iterator Tree<T>::end()
+{
+	// Итеративный алгоритм с методички
+	Iterator it;
+	it.obj = nullptr;
+	std::stack<Item*> stack;
+	stack.push(root);
+	while (!stack.empty()) {
+		Item* current = stack.top();
+		it.obj = current;
+		stack.pop();
+		if (current->right) {
+			stack.push(current->right);
+		}
+		if (current->left) {
+			stack.push(current->left);
+		}
+	}
+	return it;
+}
+
+template<typename T>
+inline Tree<T>::Item* Tree<T>::lt_Rt_t(Item*& root)
+{
+	if (!root) {
+		return nullptr;
+	}
+	lt_Rt_t(root->left);
+	lt_Rt_t(root->right);
+	return root;
+}
+
+template<typename T>
+inline T& Tree<T>::Iterator::operator*()
+{
+	return this->obj->data;
+}
+
+template<typename T>
+inline void Tree<T>::Iterator::operator++(int)
+{
+	Iterator it = *this;
+	// Возвращение к корню дерева
+	while (it.obj->parent) {
+		it.obj = it.obj->parent;
+	}
+	std::stack<Item*> stack;
+	// Помещаем корень в стек
+	stack.push(it.obj);
+	while (!stack.empty()) {
+		// Буфер текущего элемента важен
+		Item* current = stack.top();
+		// Достаём элемент, помещаем в стек его потомков
+		stack.pop();
+		if (current->right) {
+			stack.push(current->right);
+		}
+		if (current->left) {
+			stack.push(current->left);
+		}
+		// Если нашли итератор, отдаём следующему элементу в стеке
+		if (current == this->obj && stack.size()) {
+			it.obj = stack.top();
+			break;
+		}
+		else if (!stack.size()) {
+			std::cout << "Выход за пределы!\n";
+			return;
+		}
+	}
+	*this = it;
+}
+
+template<typename T>
+inline void Tree<T>::Iterator::operator--(int)
+{
+	Iterator it = *this;
+	if (it.obj->parent == nullptr) {
+		std::cout << "Выход за пределы!\n";
+		return;
+	}
+	// Возвращение к корню дерева
+	while (it.obj->parent) {
+		it.obj = it.obj->parent;
+	}
+	std::stack<Item*> stack;
+	// Помещаем корень в стек
+	stack.push(it.obj);
+	while (!stack.empty()) {
+		// Буфер текущего элемента важен
+		Item* current = stack.top();
+		// Достаём элемент, помещаем в стек его потомков
+		stack.pop();
+		if (current->right) {
+			stack.push(current->right);
+		}
+		if (current->left) {
+			stack.push(current->left);
+		}
+		// Если нашли итератор, отдаём предыдущему (буфер) элементу в стеке
+		if (stack.top() == this->obj) {
+			it.obj = current;
+			break;
+		}
+	}
+	*this = it;
+}
+
+template<typename T>
+inline bool Tree<T>::Iterator::operator==(const Iterator& it)
+{
+	if (obj == it.obj) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+template<typename T>
+inline bool Tree<T>::Iterator::operator!=(const Iterator& it)
+{
+	if (obj != it.obj) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+template<typename T>
+inline Tree<T>::ReverseIterator Tree<T>::rBegin()
+{
+	// Итеративный алгоритм с методички
+	ReverseIterator it;
+	it.obj = nullptr;
+	std::stack<Item*> stack;
+	stack.push(root);
+	while (!stack.empty()) {
+		Item* current = stack.top();
+		it.obj = current;
+		stack.pop();
+		if (current->right) {
+			stack.push(current->right);
+		}
+		if (current->left) {
+			stack.push(current->left);
+		}
+	}
+	return it;
+}
+
+template<typename T>
+inline Tree<T>::ReverseIterator Tree<T>::rEnd()
+{
+	ReverseIterator it;
+	it.obj = root;
+	return it;
+}
+
+template<typename T>
+inline T& Tree<T>::ReverseIterator::operator*()
+{
+	return this->obj->data;
+}
+
+template<typename T>
+inline void Tree<T>::ReverseIterator::operator++(int)
+{
+	ReverseIterator it = *this;
+	if (it.obj->parent == nullptr) {
+		std::cout << "Выход за пределы!\n";
+		return;
+	}
+	// Возвращение к корню дерева
+	while (it.obj->parent) {
+		it.obj = it.obj->parent;
+	}
+	std::stack<Item*> stack;
+	// Помещаем корень в стек
+	stack.push(it.obj);
+	while (!stack.empty()) {
+		// Буфер текущего элемента важен
+		Item* current = stack.top();
+		// Достаём элемент, помещаем в стек его потомков
+		stack.pop();
+		if (current->right) {
+			stack.push(current->right);
+		}
+		if (current->left) {
+			stack.push(current->left);
+		}
+		// Если нашли итератор, отдаём предыдущему (буфер) элементу в стеке
+		if (stack.top() == this->obj) {
+			it.obj = current;
+			break;
+		}
+	}
+	*this = it;
+}
+
+template<typename T>
+inline void Tree<T>::ReverseIterator::operator--(int)
+{
+	ReverseIterator it = *this;
+	// Возвращение к корню дерева
+	while (it.obj->parent) {
+		it.obj = it.obj->parent;
+	}	
+	std::stack<Item*> stack;
+	// Помещаем корень в стек
+	stack.push(it.obj);
+	while (!stack.empty()) {
+		// Буфер текущего элемента важен
+		Item* current = stack.top();
+		// Достаём элемент, помещаем в стек его потомков
+		stack.pop();
+		if (current->right) {
+			stack.push(current->right);
+		}
+		if (current->left) {
+			stack.push(current->left);
+		}
+		// Если нашли итератор, отдаём следующему элементу в стеке
+		if (current == this->obj && stack.size()) {
+			it.obj = stack.top();
+			break;
+		}
+		else if (!stack.size()) {
+			std::cout << "Выход за пределы!\n";
+			return;
+		}
+	}
+	*this = it;
+}
+
+template<typename T>
+inline bool Tree<T>::ReverseIterator::operator==(const ReverseIterator& it)
+{
+	if (obj == it.obj) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+template<typename T>
+inline bool Tree<T>::ReverseIterator::operator!=(const ReverseIterator& it)
+{
+	if (obj != it.obj) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
